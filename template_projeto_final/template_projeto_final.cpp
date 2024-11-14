4baec2b191606996b593c4bfd09200be3263e038
Lucas Pizoni Flores <lucas.324623@alunosatc.edu.br>
2024-11-13 21:01:38 -0300

    Assinaturas da classe 'ItemBblioteca' criadas

--- template_projeto_final/template_projeto_final.cpp
+++ template_projeto_final/template_projeto_final.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 class ItemBiblioteca {
 protected:
-    int id, ano;
-    string titulo;
-    bool emprestado;
+    int id_, ano_;
+    string titulo_;
+    bool emprestado_;
 
 public:
 	ItemBiblioteca(int id, string titulo, int ano);
@@ -21,8 +22,8 @@ public:
 
 class Livro : public ItemBiblioteca {
 protected:
-	string autor;
-	string genero;
+	string autor_;
+	string genero_;
 	
 public:
     Livro(int id, string titulo, int ano, string autor, string genero);      
@@ -31,8 +32,8 @@ public:
 
 class Usuario {
 protected:
-    int id, limiteEmprestimos, emprestimosAtuais;
-    string nome;
+    int id_, limiteEmprestimos_, emprestimosAtuais_;
+    string nome_;
 
 public:
     Usuario(int id, string nome, int limiteEmprestimos);
@@ -56,8 +57,8 @@ public:
 };
 
 class Biblioteca {
-    vector<Livro> livros;
-    vector<Aluno> alunos;
+    vector<Livro> livros_;
+    vector<Aluno> alunos_;
     vector<Professor> professores;
 
 public:
@@ -71,7 +72,45 @@ public:
 };
 
 int main() {
-	
+	ItemBiblioteca item(1, "Gato Maroto", 2000);
+	item.exibirDetalhes();
     return 0;
 }
 
+/*--------------------------------------------------
+Declaração de funções
+--------------------------------------------------*/
+
+/*--------------------------------------------------
+	1.1 ItemBibioteca
+--------------------------------------------------*/
+ItemBiblioteca::ItemBiblioteca(int id, string titulo, int ano):
+	id_(id), titulo_(titulo), ano_(ano), emprestado_(false){}
+		
+void  ItemBiblioteca::exibirDetalhes(){
+	cout << "---------------------------------------------------" << endl;
+	cout << "Id: " << id_ << endl;
+	cout << "Título: " << titulo_ << endl;
+	cout << "Ano: " << ano_ << endl;
+	cout << "Emprestado: " << emprestado_ << endl;
+	cout << "---------------------------------------------------" << endl;
+}
+		
+int ItemBiblioteca::getId(){
+	return id_;
+}
+		
+bool ItemBiblioteca::isEmprestado(){
+	return emprestado_;
+}
+		
+void ItemBiblioteca::emprestar(){
+	emprestado_ = true;
+}
+		
+void ItemBiblioteca::devolver(){
+	emprestado_ = false;
+}
+/*--------------------------------------------------
+	1.2 Livro
+--------------------------------------------------*/
